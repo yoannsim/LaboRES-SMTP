51cfee45500f3f708cfb5ef3b95c647988f680ad
RengokuryuuhonokaCrimsonFlame <julien.rod@heig-vd.ch>
2019-04-01 14:39:57 +0200

    Copier-coller de l'exemple des vidéos.

--- src/main/java/ch/heigvd/res/mailbot/mail/Message.java
+++ src/main/java/ch/heigvd/res/mailbot/mail/Message.java
@@ -1,4 +1,44 @@
+/**
+ * Sources :
+ * https://www.youtube.com/watch?v=OrSdRCt_6YQ&list=PLfKkysTy70Qa1IYbV9Xndojc7L-T4keF-&index=22
+ */
 package ch.heigvd.res.mailbot.mail;
 
 public class Message {
+    private String from;
+    private String[] to = new String[0];
+    private String subject;
+    private String data;
+
+    public String getFrom(){
+        return from;
+    }
+
+    public void setFrom(String from){
+        this.from = from;
+    }
+
+    public String[] getTo(){
+        return to;
+    }
+
+    public void setTo(String[] to){
+        this.to = to;
+    }
+
+    public String getSubject(){
+        return subject;
+    }
+
+    public void setSubject(String from){
+        this.subject = subject;
+    }
+
+    public String getData(){
+        return data;
+    }
+
+    public void setData(String from){
+        this.data = data;
+    }
 }
