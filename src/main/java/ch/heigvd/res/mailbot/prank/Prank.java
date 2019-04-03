db0199f3a5f72eacf289b59b8bc45b4ea02cc569
yoannsim <yoann.simonet@heig-vd.ch>
2019-04-03 13:59:51 +0200

    getMail dans prank

--- src/main/java/ch/heigvd/res/mailbot/prank/Prank.java
+++ src/main/java/ch/heigvd/res/mailbot/prank/Prank.java
@@ -1,5 +1,30 @@
 package ch.heigvd.res.mailbot.prank;
 
+import ch.heigvd.res.mailbot.mail.Group;
+import ch.heigvd.res.mailbot.mail.Message;
+
 public class Prank {
-    
+    Group group;
+    String blague;
+
+    public Prank( Group group,String blague){
+        this.blague = blague;
+        this.group = group;
+    }
+
+    public Message getMail(){
+
+        Message mail = new Message();
+        mail.setFrom(group.getEnvoyeur());
+        mail.setTo(group.getReceveurs());
+        mail.setData(blague);
+        mail.setSubject("Ma blague prefere");
+
+
+        return mail;
+    }
+
+
+
+
 }
